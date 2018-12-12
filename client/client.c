e2c44a81e9e5006c4c77252e497a7d21c40267ef
lqinggang <1944058861@qq.com>
2018-12-12 17:11:41 +0800

    send heartbeat packets regularly

--- client/client.c
+++ client/client.c
@@ -9,34 +9,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include "wrapsock.h"
 #include "crc.h"
 #include "escape.h"
 #include "protocol.h"
 
+#define ALRMTIME  30
+
+int sockfd; 
+
+static void sig_alrm(int signo);
+
+
 int main(int argc,char *argv[])
 {
-	int sockfd;
-	struct sockaddr_in servaddr;
+	//0. heartbeat package sent every 15 seconds 
+	signal(SIGALRM,sig_alrm);
+	alarm(ALRMTIME);
 
-	if((sockfd = Socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+	//1. create a socket
+	if((sockfd = Socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
 		printf("socket error\n");
 		exit(1);
 	}
 
-	char *addrbuf[sizeof(socklen_t)];
+	//2. initialize the server address
+	struct sockaddr_in servaddr;
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(ServPort);
 	servaddr.sin_addr.s_addr = inet_addr(ServAddr);
 
-	Connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	//3. connect to the server
+	Connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
 
+	//4. get message from stdin
 	char msg[MAXLINE];
-	size_t n;
-	printf("%% ");
-	while(fgets(msg, MAXLINE, stdin) != NULL) {
+	printf("%% "); //prompt
+	while(fgets(msg, MAXLINE, stdin) != NULL) {  
 		size_t length = strlen(msg);
+		//5. send the message to the server
 		psend(REPORT, sockfd, msg, length - 1, 0); //-1: not included \n
 		printf("%% ");
 		fflush(NULL);
@@ -44,4 +57,11 @@ int main(int argc,char *argv[])
 	close(sockfd);
 
 	exit(0);
+}
+
+static void sig_alrm(int signo)
+{
+	psend(HEARTBEAT, sockfd, NULL, 0, 0); //send heartbeat package
+	printf("sig_alrm\n");
+	alarm(ALRMTIME);
 }
